df3fd3d9512ee8e3b4c392d8da34ad8e3c1a2101
Max131 <max_cable@hotmail.com>
2021-05-19 15:51:17 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import {create} from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let component;
+let header;
+
+describe('<App />', () => {
+  beforeAll(() => {
+    component = create(<App />);
+    header = component.root.findByType('header');
+  });
+
+  it('Renderiza correctamente', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('Renderiza el header bien', () => {
+    expect(header).toBeDefined();
+    expect(header.props.className).toEqual('App-header');
+
+  });
+
+  it('Renderiza un texto, un link y una imagen', () => {
+    const img = header.findByType('img');
+    const a = header.findByType('a');
+    const p = header.findByType('p');
+
+    expect(img).toBeDefined();
+    expect(img.props.className).toEqual('App-logo');
+    expect(img.props.src).toEqual('logo.svg');
+
+    expect(a).toBeDefined();
+    expect(a.props.href).toBe('https://reactjs.org');
+
+    expect(p).toBeDefined();
+    expect(p.findByType('code')).toBeDefined();
+  })
 });
